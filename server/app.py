e1f0324b0cad25b53fb2fdb4a4c98d4babf942aa
Krishna Verma <krishnaias1289@gmail.com>
2026-04-09 01:30:16 +0530

    Make server module directly callable

--- server/app.py
+++ server/app.py
@@ -11,3 +11,7 @@ def main() -> None:
     host = os.getenv("HOST", "0.0.0.0")
     port = int(os.getenv("PORT", "7860"))
     uvicorn.run("server.app:app", host=host, port=port)
+
+
+if __name__ == "__main__":
+    main()
